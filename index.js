f27b804b2c3ee2bb83ea24bc1be403e8f4d733ea
naimurnemu <naimurrahaman82@gmail.com>
2021-12-26 06:10:34 +0600

    server done

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
-const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
@@ -11,10 +10,6 @@ const port = process.env.PORT || 5000;
 // Http Middleware
 app.use(cors());
 app.use(express.json());
-
-// Parses text as url and json
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
-app.use(bodyParser.json({ limit: "10mb", extended: true }));
 
 // Database connecting URL
 const uri = `mongodb+srv://${process.env.MEMORY_DB_USER}:${process.env.MEMORY_DB_PASS}@cluster0.amqnd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
